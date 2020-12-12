ba7387ceab13c2759c0dcd65ce4a1347371f7dab
yanbin <yanb25@mail2.sysu.edu.cn>
2020-12-12 14:03:13 +0800

    add std::cout vector

--- include/helloworld/common.hpp
+++ include/helloworld/common.hpp
@@ -1,5 +1,8 @@
 #ifndef COMMON_H
 #define COMMON_H
+#include <iostream>
+#include <vector>
+#include <string>
 
 // TODO: make it portable.
 // #if !define __builtin_expect
@@ -14,5 +17,43 @@
 #if !defined(unlikely)
 #define unlikely(x) (__builtin_expect(!!(x), 0))
 #endif
+
+namespace hello
+{
+namespace debug
+{
+template <typename T>
+std::ostream& operator<<(std::ostream& os, const std::vector<T>& vec)
+{
+    os << "[";
+    for (size_t i = 0; i < vec.size(); ++i)
+    {
+        os << vec[i];
+        if (i != vec.size() - 1)
+        {
+            os << ", ";
+        }
+    }
+    os << "]";
+    return os;
+}
+std::ostream& operator<<(std::ostream& os, const std::vector<std::string>& vec)
+{
+    os << "[";
+    for (size_t i = 0; i < vec.size(); ++i)
+    {
+        os << "\"" << vec[i] << "\"";
+        if (i != vec.size() - 1)
+        {
+            os << ", ";
+        }
+    }
+    os << "]";
+    return os;
+}
+
+}
+}
+
 
 #endif
