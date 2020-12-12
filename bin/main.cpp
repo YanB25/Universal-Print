48d7ba22f77e3731e19c28352ed3e8ef348a0f32
yanbin <yanb25@mail2.sysu.edu.cn>
2020-12-12 12:44:19 +0800

    finish DLOG family

--- bin/main.cpp
+++ bin/main.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
+#include <memory>
 
 #include "helloworld/debug.hpp"
 #include "helloworld/helloworld.hpp"
+#include "helloworld/cxxdebug.hpp"
 int dead_loop()
 {
     while (true)
@@ -19,5 +21,32 @@ int main()
             dead_loop());
     check(true, "It's a check");
     dcheck(true, "It's a debug check");
+
+    LOG(INFO) << "!!!";
+    LOG(INFO);
+    LOG(WARNING) << "hei!";
+    LOG(ERROR) << "hei!";
+    LOG_IF(INFO, true) << "true!";
+    LOG_IF(INFO, false) << dead_loop();
+    std::shared_ptr<int> p;
+    LOG_IF(INFO, p) << "pointer !";
+    p = std::make_shared<int>(5);
+    LOG_IF(INFO, p) << "pointer again!";
+    for (int i = 0; i < 100; ++i)
+    {
+        LOG_EVERY_N(INFO, 20) << "Log every 20!";
+        LOG_EVERY_N(INFO, 20) << "Log every again 20!";
+        LOG_EVERY_N(INFO, 20) << "Log every hei 20!";
+        LOG_IF_EVERY_N(INFO, true, 20) << "IF: Log every hei 20!";
+        LOG_IF_EVERY_N(INFO, false, 20) << "false: Log every hei 20!";
+        LOG_FIRST_N(INFO, 3) << "only first 3";
+    }
+
+    DLOG(INFO) << "Dlog info";
+    DLOG(WARNING) << "Dlog info";
+    DLOG(ERROR) << "Dlog info";
+    int* pn = nullptr;
+    DLOG(INFO) << "It's " << *pn;
+    DLOG(INFO) << "It's " << dead_loop();
     return 0;
 }
---
+++ include/helloworld/cxxdebug.hpp
@@ -0,0 +1,116 @@
+#ifndef CXXDEBUG_H
+#define CXXDEBUG_H
+#include <inttypes.h>
+
+#include <iostream>
+
+#ifndef NDEBUG
+constexpr bool is_debug = true;
+#else
+constexpr bool is_debug = false;
+#endif
+
+// This class is used to explicitly ignore values in the conditional
+// logging macros.  This avoids compiler warnings like "value computed
+// is not used" and "statement has no effect".
+class LogMessageVoidify
+{
+public:
+    LogMessageVoidify()
+    {
+    }
+    // This has to be an operator with a precedence lower than << but
+    // higher than ?:
+    void operator&(std::ostream &)
+    {
+    }
+};
+
+static constexpr int INFO = 0, WARNING = 1, ERROR = 2, FATAL = 3;
+static constexpr const char *severity_level[] = {
+    "INFO", "WARNING", "ERROR", "PANIC"};
+
+class Stream
+{
+public:
+    Stream(const int severity, const char *file, size_t line)
+        : severity_(severity), file_(file), line_(line)
+    {
+    }
+    std::ostream &stream()
+    {
+        return std::cerr << "[" << severity_level[severity_] << "] " << file_
+                         << ":" << line_ << " ";
+    }
+    ~Stream()
+    {
+        std::cerr << std::endl;
+        if constexpr (is_debug)
+        {
+            if (severity_ == FATAL)
+            {
+                std::terminate();
+            }
+        }
+    }
+
+private:
+    int severity_;
+    const char *file_;
+    size_t line_;
+};
+
+// Use macro expansion to create, for each use of LOG_EVERY_N(), static
+// variables with the __LINE__ expansion as part of the variable name.
+#define LOG_EVERY_N_VARNAME(base, line) LOG_EVERY_N_VARNAME_CONCAT(base, line)
+#define LOG_EVERY_N_VARNAME_CONCAT(base, line) base##line
+
+#define LOG(severity) Stream(severity, __FILE__, __LINE__).stream()
+#define LOG_IF(severity, cond) \
+    (!cond) ? void(0) : LogMessageVoidify() & LOG(severity)
+
+#define LOG_OCCURRENCES LOG_EVERY_N_VARNAME(occurrences_, __LINE__)
+#define LOG_OCCURRENCES_MOD_N LOG_EVERY_N_VARNAME(occurrences_mod_n_, __LINE__)
+
+#define LOG_EVERY_N(severity, n)             \
+    static size_t LOG_OCCURRENCES_MOD_N = 0; \
+    if (++LOG_OCCURRENCES_MOD_N > n)         \
+        LOG_OCCURRENCES_MOD_N -= n;          \
+    if (LOG_OCCURRENCES_MOD_N == 1)          \
+    LOG(severity)
+
+#define LOG_IF_EVERY_N(severity, cond, n)                                     \
+    static size_t LOG_OCCURRENCES_MOD_N = 0;                                  \
+    if (cond && ((LOG_OCCURRENCES_MOD_N = (LOG_OCCURRENCES_MOD_N + 1) % n) == \
+                 (1 % n)))                                                    \
+    LOG(severity)
+
+#define LOG_FIRST_N(severity, n)         \
+    static size_t LOG_OCCURRENCES_N = 0; \
+    if (++LOG_OCCURRENCES_N <= n)        \
+    LOG(severity)
+
+#ifdef NDEBUG
+// release
+#define DLOG(severity) \
+    static_cast<void>(0), true ? (void) 0 : LogMessageVoidify() & std::cerr
+#define DLOG_EVERY_N(severity, n) \
+    static_cast<void>(0), true ? (void) 0 : LogMessageVoidify() & std::cerr
+#define DLOG_IF(severity, cond) \
+    static_cast<void>(0), true ? (void) 0 : LogMessageVoidify() & std::cerr
+#define DLOG_IF_EVERY_N(severity, cond, n) \
+    static_cast<void>(0), true ? (void) 0 : LogMessageVoidify() & std::cerr
+#define DLOG_FIRST_N(severity, n) \
+    static_cast<void>(0), true ? (void) 0 : LogMessageVoidify() & std::cerr
+
+#else
+// debug
+#define DLOG(severity) LOG(severity)
+#define DLOG_EVERY_N(severity, n) LOG_EVERY_N(severity, n)
+#define DLOG_IF(severity, cond) LOG_IF(severity, cond)
+#define DLOG_IF_EVERY_N(severity, cond, n) LOG_IF_EVERY_N(severity, cond, n)
+#define DLOG_FIRST_N(severity, n) LOG_FIRST_N(severity, n)
+
+#endif
+
+#endif
