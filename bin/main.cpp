02492a0a2fa0fc0fa8a99c0cd8975bc2c07e71d3
Bin Yan <yanb20@mails.tsinghua.edu.cn>
2022-03-27 11:22:43 +0800

    Revert "trivial"
    This reverts commit 06f2729723891e5f3df4f42e63bc1d313b5be05f.

--- bin/main.cpp
+++ bin/main.cpp
@@ -2,14 +2,14 @@
 
 DEFINE_string(msg, "helloworld", "The message to print");
 
-int main(int argc, char *argv[])
+int main(int argc, char* argv[])
 {
     google::InitGoogleLogging(argv[0]);
     gflags::ParseCommandLineFlags(&argc, &argv, true);
     FLAGS_logtostderr = true;
     FLAGS_colorlogtostderr = true;
-
+    
     LOG(INFO) << FLAGS_msg;
-
+    
     return 0;
 }
